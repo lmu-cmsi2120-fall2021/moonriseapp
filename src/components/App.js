ad9804fb1a00b0cc135c4307cfb403427fdfae2f
BryMad <75340105+BryMad@users.noreply.github.com>
2022-12-06 12:02:40 -0800

    getting year of moonrises (but date math needs work)

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Entry from "./Entry.js";
 import MoonriseList from "./MoonriseList.js";
 import Header from "./Header.js";
+import dayjs from "dayjs";
 
 export default function App() {
   const [days, setDays] = useState([]);
@@ -21,11 +22,38 @@ export default function App() {
     )
       .then((response) => response.json())
       .then((data) => {
-        setDays(data.days);
-        console.log(data);
+        const moonrises = addDays(data.days);
+        setDays(moonrises);
+        // console.log(data);
       })
       .catch((error) => console.log(error));
   }
+
+  function addDays(dayArray) {
+    // create additional days
+    // do math and add moonrise
+    const moonrises = [];
+    let day = dayArray[0];
+    if (day) {
+      moonrises.push(day);
+      for (let i = 0; i < 364; i++) {
+        const moonriseTime = dayjs(`${day.datetime}T${day.moonrise}`)
+          .add(50, "minute")
+          .format("hh:mm:ss");
+        const newDate = dayjs(day.datetime).add(1, "day").format("YYYY-MM-DD");
+        const newDay = {
+          datetime: newDate,
+          moonrise: moonriseTime,
+        };
+        day = newDay;
+        moonrises.push(newDay);
+      }
+      console.log("AddDays: ", moonrises);
+      return moonrises;
+    }
+  }
+
+  addDays(days);
 
   return (
     <div className="App">
